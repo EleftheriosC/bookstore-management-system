da2bef47a49222caa020e066690009b82df65dfa
EleftheriosC <celeftherios@hotmail.com>
2023-11-02 21:15:09 +0200

    commit

--- bookstore-fe/src/App.tsx
+++ bookstore-fe/src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@mui/material";
 
-function App() {
+function LoginPage() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+
+        <Button>Log In</Button>
+
+        <Button>Register</Button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Helloooo
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default LoginPage;
--- bookstore-fe/src/index.tsx
+++ bookstore-fe/src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoginPage from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+
+      <LoginPage/>
+    {/*<App />*/}
+
+
   </React.StrictMode>
 );
 
