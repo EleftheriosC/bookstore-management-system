c42063174971565115abbd3bccf37aae594c2620
EleftheriosC <celeftherios@hotmail.com>
2023-11-02 21:57:55 +0200

    commit

--- bookstore-fe/src/index.tsx
+++ bookstore-fe/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Pages/App';
 import reportWebVitals from './reportWebVitals';
-import LoginPage from "./App";
+import LoginPage from "./Pages/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
