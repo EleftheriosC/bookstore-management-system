887202eb3ac9e84243f3eaf5863c3698ffc8987a
EleftheriosC <celeftherios@hotmail.com>
2023-11-03 19:42:51 +0200

    commit

--- bookstore-fe/src/pages/Bookstore.js
+++ bookstore-fe/src/pages/Bookstore.js
@@ -3,8 +3,37 @@ import Login from "../components/Login";
 import {Box, Button, Grid} from "@mui/material";
 import Welcome from "../components/Welcome";
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import api from '../api/books';
 
 function Bookstore() {
+
+    const [books, setBooks] = useState([]);
+    const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            try {
+                const response = await api.get('/Book/GetBooks');
+                if (response && response.data){
+                    setBooks(response.data);
+                }
+            } catch (err) {
+                if (err.response) {
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                }
+                else {
+                    console.log(`Error: ${err.message}`)
+                }
+            }
+        }
+
+        fetchBooks();
+    }, []);
+
   return (
     <main className={"App"}>
         <Grid container>
