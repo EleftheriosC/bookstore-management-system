f116d5663bed84c98c8623df2e27756d7b94a7be
EleftheriosC <celeftherios@hotmail.com>
2023-11-03 16:33:21 +0200

    commit

--- bookstore-fe/src/components/Login.js
+++ bookstore-fe/src/components/Login.js
@@ -1,11 +1,42 @@
-import {Grid} from "@mui/material";
+import {Box, Button, Grid, Input, TextField, Typography} from "@mui/material";
+import {red} from "@mui/material/colors";
 
 function Login() {
   return (
       <>
-          <Grid>
-              HI
+          <Grid container>
 
+              <Grid item xs={12} >
+                  <Typography variant={"h5"}>
+                      Log In
+                  </Typography>
+              </Grid>
+
+              <Grid item xs={12}>
+
+                  <TextField placeholder={'Username'} >
+                      Input 1
+                  </TextField>
+              </Grid>
+
+              <Grid item xs={12}>
+                  <TextField placeholder={'Password'}>
+                      Input 2
+                  </TextField>
+              </Grid>
+
+              <Grid item xs={12}>
+                  <Typography variant={"h6"}>
+                  No Account?
+                  </Typography>
+              </Grid>
+
+
+              <Grid item xs={12}>
+                  <Button>
+                      Register
+                  </Button>
+              </Grid>
 
           </Grid>
       </>
