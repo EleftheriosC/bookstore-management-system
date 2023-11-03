40bccc960993b233143c6cccb89dad008eb026dd
EleftheriosC <celeftherios@hotmail.com>
2023-11-03 22:56:16 +0200

    commit

---
+++ bookstore-fe/src/pages/Home.js
@@ -0,0 +1,21 @@
+import '../App.css';
+import Login from "../components/Login";
+import {Box, Grid} from "@mui/material";
+import Welcome from "../components/Welcome";
+
+function Home() {
+  return (
+    <main className={"App"}>
+        <Grid container>
+            <Grid item xs={6}>
+                <Welcome/>
+            </Grid>
+            <Grid item xs={6}>
+                <Login/>
+            </Grid>
+        </Grid>
+    </main>
+  );
+}
+
+export default Home;
