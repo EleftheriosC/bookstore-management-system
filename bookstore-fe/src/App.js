80d1d8053262b751507d45d9c2d38c305d4789cf
EleftheriosC <EleftheriosC@users.noreply.github.com>
2023-11-03 18:01:12 +0200

    Create login page (#2)

--- bookstore-fe/src/App.js
+++ bookstore-fe/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Login from "./components/Login";
+import {Box, Grid} from "@mui/material";
+import WelcomePage from "./components/WelcomePage";
 
 function App() {
   return (
     <main className={"App"}>
-      <Login/>
+        <Grid container>
+            <Grid item xs={6}>
+                <WelcomePage/>
+            </Grid>
+            <Grid item xs={6}>
+                <Login/>
+            </Grid>
+
+        </Grid>
     </main>
   );
 }
--- bookstore-fe/src/components/Login.js
+++ bookstore-fe/src/components/Login.js
@@ -1,13 +1,46 @@
-import {Grid} from "@mui/material";
+import {Box, Button, Grid, Input, TextField, Typography} from "@mui/material";
+import {red} from "@mui/material/colors";
 
 function Login() {
   return (
       <>
-          <Grid>
-              HI
+          <header className="Login-header">
+
+          <Grid container>
+
+              <Grid item xs={12} >
+                  <Typography variant={"h5"} color={'black'}>
+                      Log In
+                  </Typography>
+              </Grid>
+
+              <Grid item xs={12}>
+
+                  <TextField placeholder={'Username'} >
+                      Input 1
+                  </TextField>
+              </Grid>
+
+              <Grid item xs={12}>
+                  <TextField placeholder={'Password'}>
+                      Input 2
+                  </TextField>
+              </Grid>
+
+              <Grid item xs={12}>
+                  <Typography variant={"h6"} color={'black'}>
+                  No Account?
+                  </Typography>
+              </Grid>
 
 
+              <Grid item xs={12}>
+                  <Button>
+                      Register
+                  </Button>
+              </Grid>
           </Grid>
+          </header>
       </>
   );
 }
