fd25258bceab9cef5aa47c2840473da179158c24
EleftheriosC <celeftherios@hotmail.com>
2023-11-06 19:15:17 +0200

    added logs

--- user-service/user-service/Services/AuthenticationService.cs
+++ user-service/user-service/Services/AuthenticationService.cs
@@ -11,17 +11,22 @@ namespace user_service.Services
     {
         private readonly UserManager<UserIdentity> _userManager;
         private readonly IConfiguration _configuration;
+        private readonly ILogger<AuthenticationService> _logger;
 
         public AuthenticationService(
             UserManager<UserIdentity> userManager,
-            IConfiguration configuration)
+            IConfiguration configuration,
+            ILogger<AuthenticationService> logger)
         {
             _userManager = userManager;
             _configuration = configuration;
+            _logger = logger;
         }
 
         public async Task<string> Register(RegisterRequest registerRequest)
         {
+            _logger.LogInformation("Attempting to register user");
+
             var userByEmail = await _userManager.FindByNameAsync(registerRequest.Email);
             var userByUsername = await _userManager.FindByNameAsync(registerRequest.Username);
 
@@ -29,6 +34,7 @@ namespace user_service.Services
 
             if (userExists)
             {
+                _logger.LogError($"User already registered with with email {registerRequest.Email} or username {registerRequest.Username}");
                 throw new ArgumentException($"User with email {registerRequest.Email} or username {registerRequest.Username} already exists.");
             }
 
@@ -43,6 +49,7 @@ namespace user_service.Services
 
             if (!response.Succeeded)
             {
+                _logger.LogError($"User registration failed with error {IsoalteErros(response.Errors)}");
                 throw new ArgumentException($"Unable to register user with {registerRequest.Username} with error {IsoalteErros(response.Errors)} already exists.");
             }
 
@@ -51,16 +58,20 @@ namespace user_service.Services
                 Username = registerRequest.Email,
                 Password = registerRequest.Password,
             };
+            _logger.LogInformation($"User with username {registerRequest.Username} registered succesfully");
 
             return await Login(loginRequest);
         }
 
         public async Task<string> Login(LoginRequest loginRequest)
         {
+            _logger.LogInformation("Attempting to login user");
+
             var userIdentity = await _userManager.FindByNameAsync(loginRequest.Username);
 
             if (userIdentity == null)
             {
+                _logger.LogInformation("No user found by username, searching by email");
                 userIdentity = await _userManager.FindByEmailAsync(loginRequest.Username);
             }
 
@@ -68,6 +79,7 @@ namespace user_service.Services
 
             if (userFailedAuthentication)
             {
+                _logger.LogError($"User {loginRequest.Username} authentication failed");
                 throw new ArgumentException($"Unable to authenticate user with username {loginRequest.Username}");
             }
 
@@ -79,6 +91,8 @@ namespace user_service.Services
             };
 
             var token = GenerateToken(authClaims);
+
+            _logger.LogInformation($"User with username {loginRequest.Username} authenticated succesfully");
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
--- user-service/user-service/Services/BookService.cs
+++ user-service/user-service/Services/BookService.cs
@@ -3,9 +3,17 @@
     public class BookService : IBookService
     {
         private readonly BookDbContext _bookContext = new();
+        private readonly ILogger<BookService> _logger;
+
+        public BookService(ILogger<BookService> logger)
+        {
+            _logger = logger;
+        }
 
         public async Task CreateBookEntry(Book book)
         {
+            _logger.LogInformation("Attempting to add new book entry");
+
             await _bookContext.AddAsync(new BookEntity
             {
                 Title = book.Title,
@@ -14,15 +22,20 @@
                 ISBN = book.ISBN
             });
             await _bookContext.SaveChangesAsync();
+            _logger.LogInformation($"Book added");
         }
 
         public IEnumerable<BookEntity> GetAllBooks()
         {
+            _logger.LogInformation("Retrieving all books");
+
             return _bookContext.Books.OrderBy(b => b.BookEntityId);
         }
 
         public IEnumerable<BookEntity> GetBooksByTitleOrAuthor(string? title, string? author)
         {
+            _logger.LogInformation($"Retrieving books with title {title} and author {author}");
+
             var query = _bookContext.Books.AsQueryable();
             var books = new List<BookEntity>();
             if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(author)) {
@@ -49,6 +62,8 @@
 
         public async Task UpdateBook(int bookId, Book updatedBook)
         {
+            _logger.LogInformation($"Attempting to update book with Id {bookId}");
+
             var book = _bookContext.Books.Find(bookId);
             if (book is not null)
             {
@@ -57,16 +72,31 @@
                 book.PublicationYear = (int)(updatedBook.PublicationYear > 0 ? updatedBook.PublicationYear : book.PublicationYear);
                 book.ISBN = updatedBook.ISBN == "" ? book.ISBN : updatedBook.ISBN;
                 await _bookContext.SaveChangesAsync();
+                _logger.LogInformation($"Book with Id {bookId} updated");
+            }
+            else
+            {
+                _logger.LogError($"Book with id {bookId} not found");
+                throw new ArgumentException($"Book with id {bookId} not found");
             }
         }
 
         public async Task DeleteBook(int bookId)
         {
+            _logger.LogInformation($"Attempting to delete book with Id {bookId}");
+
             var book = _bookContext.Books.Find(bookId);
             if (book is not null)
             {
                 _bookContext.Remove(book);
                 await _bookContext.SaveChangesAsync();
+                _logger.LogInformation($"Book with Id {bookId} deleted");
+
+            }
+            else
+            {
+                _logger.LogError($"Book with id {bookId} not found");
+                throw new ArgumentException($"Book with id {bookId} not found");
             }
         }
 
