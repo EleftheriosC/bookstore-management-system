d94a2cf9544052d49bdd3005e02314a1e8655d15
EleftheriosC <celeftherios@hotmail.com>
2023-11-07 18:11:24 +0200

    commit

--- bookstore-fe/src/App.js
+++ bookstore-fe/src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import Login from "./components/Login";
-import {Box, Grid} from "@mui/material";
-import Welcome from "./components/Welcome";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Bookstore from "./pages/Bookstore";
